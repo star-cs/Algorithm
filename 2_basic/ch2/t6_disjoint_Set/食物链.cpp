e6327b4357f9ed6003fb3f5684076703d1ade145
下次我请客 <2572523998@qq.com>
2023-01-11 20:17:33 +0800

    ch2 数据结构2完结

--- 2_basic/ch2/t6_disjoint_Set/食物链.cpp
+++ 2_basic/ch2/t6_disjoint_Set/食物链.cpp
@@ -23,11 +23,8 @@ A 吃 B，B 吃 C，C 吃 A。
 
 输入格式
 第一行是两个整数 N 和 K ，以一个空格分隔。
-
 以下 K 行每行是三个正整数 D，X，Y，两数之间用一个空格隔开，其中 D 表示说法的种类。
-
 若 D=1 ，则表示 X 和 Y 是同类。
-
 若 D=2 ，则表示 X 吃 Y。
 
 输出格式
@@ -36,6 +33,75 @@ A 吃 B，B 吃 C，C 吃 A。
 1≤N≤50000,0≤K≤100000
 */
 
+
+/*
+记录每个点到根节点的关系<距离>
+
+    根节点
+      | 
+      A     距离1   A吃根节点
+      |
+      B     距离2   B吃A    B被C吃
+      |
+      C     距离3   C和根节点是同类
+
+距离 mod 3      1       可以吃根节点
+                2       可以被根节点吃
+                0       和根节点同类       
+
+*/
 #include <iostream>
 using namespace std;
 const int N = 500010;
+int n , m;
+int p[N] , d[N];
+
+int find(int x)
+{
+    if(p[x] != x){
+        int t = find(p[x]);
+        d[x] += d[p[x]];
+        p[x] = t;
+    }
+    return p[x];
+}
+
+int main()
+{
+    scanf("%d%d" , &n , &m);
+
+    for(int i = 1 ; i <= N ; i++)   p[i] = i;
+
+    int res = 0;
+    while(m--)
+    {
+        int t , x , y;
+        scanf("%d%d%d" , &t , &x , &y);
+        if(x > n || y > n) res++;
+        else
+        {
+            int px = find(x) , py = find(y);
+            if(t == 1)
+            {
+                // px == py 在同一树上
+                if(px == py && (d[x] - d[y]) % 3 != 0)  res++;
+                else if(px != py)
+                {
+                    p[px] = py;
+                    d[px] = d[y] - d[x];
+                }
+            }
+            else
+            {
+                if(px == py && (d[x] - d[y] - 1) % 3 != 0) res++;
+                else if(px != py)
+                {
+                    p[px] = py;
+                    d[px] = d[y] - d[x] + 1;
+                } 
+            }
+        }
+    }
+    printf("%d" , res);
+    return 0;
+}
