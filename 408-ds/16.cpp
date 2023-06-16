b7f7337208e5181581f2ddd3769ddc81bacdec2d
下次我请客 <2572523998@qq.com>
2023-06-16 19:37:23 +0800

    16年

---
+++ 408-ds/16.cpp
@@ -0,0 +1,49 @@
+/*
+    |n1-n2|最小，两个集合尽量相等
+    |S1-S2|最大，A集合最大值和最小值分开
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void _sort(int arr[] , int l , int r)
+{
+    if(l >= r)   return;
+    int x = arr[l] , i = l-1 , j = r+1;
+    while(i < j)
+    {
+        while(arr[++i] < x) {}
+        while(arr[--j] > x) {}
+        if(i < j) swap(arr[i] , arr[j]);
+    }
+    _sort(arr , l , j);
+    _sort(arr , j+1 , r);
+}
+
+int fun(int arr[] , int len)
+{
+    //排序 快排
+    _sort(arr , 0 , len-1);
+
+    int mid = len / 2;
+
+    int i = 0 , s1 = 0 , s2 = 0 ;
+    while (i <= mid) {
+        s1 += arr[i++];
+    }
+    while (i < len) {
+        s2 += arr[i++];
+    }
+
+    return s2 - s1;
+}
+
+int main()
+{
+    int A[10] = {1,2,3,4,5,657,7676,75,8,876};
+    _sort(A , 0 , 9);
+    for(int a : A)
+        cout << a << " ";
+    cout << endl;
+    cout << fun(A , 10);
+}
