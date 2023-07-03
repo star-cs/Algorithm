f1f4fb5073045b94dc7075bc3bba76162b1d8c8a
下次我请客 <2572523998@qq.com>
2023-07-03 18:13:26 +0800

    day3

--- 大师兄/item.cpp
+++ 大师兄/item.cpp
@@ -1,10 +1,12 @@
  #include <stdio.h>
  #include <stdlib.h>
  
+ typedef int ElemType;
+
 //Day01
 //在带头结点的单链表L中,删除所有值为x的结点,并释放其空间,假设值为x的结点不唯一
 //编写算法以实现上述操作
-void del_x(LinkList &L , ElemType x)
+void Del_x(LinkList &L , ElemType x)
 {
     LNode *pre = L , *cnt = L->next;
     while(cnt != NULL)  
@@ -42,4 +44,30 @@ void Merge(SqlList A , SqlList B , SqlList &C)
     while(j < B.length) C.data[z++] = B.data[j++];
     C.length = z + 1;
     return true;
+}
+
+
+//Day03
+//设计一个高效算法,将顺序表L的所有元素逆置,要求算法的空间复杂度为O(1)
+// 1 2 3 4 5 6
+// 5 4 3 2 1
+void Reverse(SqlList &A)
+{
+    int i = 0 ,  j = A.length - 1; 
+    while(i < j)
+    {
+        ElemType t = A.data[i];
+        A.data[i] = A.data[j];
+        A.data[j] = t;
+        i++ , j--;
+    }
+}
+//递归法
+void R2(SqlList &A, int left , int right)
+{
+    if(left < right)
+    {
+        swap(A.data[left] , A.data[right]);
+        R2(A , left+1 , right-1);
+    }
 }
